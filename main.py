8eff73de5b578ae24187de1547c308cad318ab85
Vitor <vitorluciopadilhaoliveira4@gmail.com>
2026-05-24 16:57:43 -0400

    import da bliblioteca random

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from torch.optim import SGD
 from torch.nn import CrossEntropyLoss
 import numpy as np
 import argparse
+import random
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--epocas", default=16, required=False, type=int)
